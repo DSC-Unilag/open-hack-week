973c73b0680c97bc8e07b8c88bc68d2a74d1185d
timzprof <timilehin.olumofin@gmail.com>
2020-06-28 22:11:31 +0100

    Removed Overlay

--- v2/js/app.js
+++ v2/js/app.js
@@ -1,13 +1,10 @@
-const overlay = document.querySelector('.overlay');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileNav = document.querySelector('.mobile-menu__nav');
 
 const toggleMobileNav = () => {
   if (mobileNav.classList.contains('hide')) {
-    overlay.classList.remove('hide');
     mobileMenu.classList.add('open');
   } else {
-    overlay.classList.add('hide');
     mobileMenu.classList.remove('open');
   }
   mobileNav.classList.toggle('hide');
